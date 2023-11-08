a4b58932ae74e0f3a479c28595f7a791b913a503
Mert Dogan <mert.dogan@logo.com.tr>
2023-11-08 14:16:39 +0300

    index.tsx and index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,23 @@
+declare module 'diva-mobile-vision-camera-mrz-scanner' {
+  export {
+    MRZCamera,
+    MRZScanner,
+    MRZProperties,
+    BoundingFrame,
+    Dimensions,
+    MRZCameraProps,
+    MRZFrame,
+    MRZScannerProps,
+    OCRElement,
+    Point,
+    Rect,
+    Size,
+    Text,
+    TextBlock,
+    TextElement,
+    TextLine,
+    boundingBoxAdjustToView,
+    sortFormatsByResolution,
+    scanMRZ,
+  } from './src/index';
+}
